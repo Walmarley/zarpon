d21727fcf48fe94e2b2709cb37bb04042a77207b
Walmarley <walmarley22@hotmail.com>
2025-03-08 15:01:20 -0300

    save point

---
+++ app/Controllers/Http/AddressController.js
@@ -0,0 +1,87 @@
+'use strict'
+
+const Address = use('App/Models/Address')
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with addresses
+ */
+class AddressController {
+  /**
+   * Show a list of all addresses.
+   * GET addresses
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index () {
+    const addresses = await Address.query().with('user').fetch()
+    return addresses
+  }
+
+  /**
+   * Create/save a new address.
+   * POST addresses
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, auth }) {
+    const data = request.only(['user_id', 'content'])
+    const address = await Address.create({ user_id: auth.user.id, ...data })
+
+    return address
+  }
+
+  /**
+   * Display a single address.
+   * GET addresses/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params}) {
+    const address = await Address.findOrFail(params.id)
+
+    return address
+  }
+
+  /**
+   * Update address details.
+   * PUT or PATCH addresses/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+  }
+
+  /**
+   * Delete a address with id.
+   * DELETE addresses/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, auth, response }) {
+    const address = await Address.findOrFail(params.id)
+
+    if (address.user_id !== auth.user.id) {
+      return response.status(401).send({ error: 'Not authorized' })
+    }
+
+    await address.delete()
+  }
+}
+
+module.exports = AddressController
---
+++ database/migrations/1741450313721_address_schema.js
@@ -0,0 +1,28 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class AddressSchema extends Schema {
+  up () {
+    this.create('addresses', (table) => {
+      table.increments()
+      table
+        .integer('user_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+      table.string('content', 255).notNullable()
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('addresses')
+  }
+}
+
+module.exports = AddressSchema
